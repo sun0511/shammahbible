882a15d10a1404a7bd9f2f06a13511bd440e80e5
sun0511 <sun0511777@gmail.com>
2019-10-24 14:50:31 +0900

    second

--- bible/urls.py
+++ bible/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import  re_path ,path
+from django.urls import  re_path, path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^first/',include('first.urls'))
 ]
