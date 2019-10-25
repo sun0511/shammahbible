bfca622bb8a40777f7c452f7fdca42f88d39cc20
sun0511 <sun0511777@gmail.com>
2019-10-25 14:22:38 +0900

    graphql

--- bible/urls.py
+++ bible/urls.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from django.urls import  re_path, path, include
 from graphene_django.views import GraphQLView
 from bible.schema import schema
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^graphql/$', GraphQLView.as_view(graphiql=True, schema=schema))
+     re_path(r'^graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
