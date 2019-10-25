e3824b0fa35cceee3c0d147fb05fba2b3b060cb8
sun0511 <sun0511777@gmail.com>
2019-10-25 13:31:04 +0900

    graphql

--- bible/urls.py
+++ bible/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import  re_path, path, include
+from graphene_django.views import GraphQLView
+from bible.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^first/',include('first.urls'))
+    re_path(r'^graphql$', GraphQLView.as_view(graphiql=True, schema=schema))
 ]
--- first/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def post_list(request):
-    return render(request, 'first/post_list.html')
