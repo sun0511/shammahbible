052e7fdebe921480559c2720dd457f54f8aa1c11
sun0511 <sun0511777@gmail.com>
2019-10-25 14:25:24 +0900

    graphql

--- bible/settings/common.py
+++ bible/settings/common.py
@@ -62,7 +62,9 @@ ROOT_URLCONF = 'bible.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'bible', 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- bible/settings/prod.py
+++ bible/settings/prod.py
@@ -1,4 +1,5 @@
 from .common import *
 
 DEBUG = True
+CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = ['keywordbible.pythonanywhere.com']
