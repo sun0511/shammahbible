40c78c7cd2958d2cff217d6620710d89d0d5e6d1
sun0511 <sun0511777@gmail.com>
2019-10-25 14:24:14 +0900

    graphql

--- bible/settings/common.py
+++ bible/settings/common.py
@@ -62,9 +62,7 @@ ROOT_URLCONF = 'bible.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'bible', 'templates')
-        ],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
