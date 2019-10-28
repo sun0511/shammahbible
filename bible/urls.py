f419e142f7b2c54c5b25ed4f67843632f3d6acd7
sun0511 <sun0511777@gmail.com>
2019-10-28 17:33:37 +0900

    graphql

--- bible/urls.py
+++ bible/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import  re_path, path, include
 from graphene_django.views import GraphQLView
 from bible.schema import schema
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     re_path(r'^graphql/$', GraphQLView.as_view(graphiql=True, schema=schema)),
+    re_path(r'^graphql/', GraphQLView.as_view(graphiql=True, schema=schema)),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
