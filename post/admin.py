e2e1af5a378a9db3d11bd50690b5d71379a934ba
sun0511 <sun0511777@gmail.com>
2019-10-31 17:07:40 +0900

    last

--- post/admin.py
+++ post/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Post
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title' ,'text', 'created_At', 'updated_At']
+    list_display = ['title', 'created_At', 'updated_At']
 
 admin.site.register(Post, PostAdmin)
