88ccf10431ef0f5ea1bb5d5dd517d7501748276f
sun0511 <sun0511777@gmail.com>
2019-10-31 16:37:05 +0900

    last

--- post/models.py
+++ post/models.py
@@ -4,8 +4,8 @@ class Post(models.Model):
     objects = models.Manager()
     title = models.CharField(max_length=30)
     image = models.ImageField(blank=True)
-    text = models.TextField()
-    audio = models.FileField()
+    text = models.TextField(blank=True)
+    audio = models.FileField(blank=True)
     created_At = models.DateTimeField(auto_now_add=True)
     updated_At = models.DateTimeField(auto_now=True)
     def __str__(self):
